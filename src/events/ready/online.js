76e3511f5a6b8d9be785ddefa954d69dea1f486e
Leonardo de Martini <demartinileo@hotmail.com>
2023-02-09 23:28:16 -0300

    Random status

--- src/events/ready/online.js
+++ src/events/ready/online.js
@@ -1,10 +1,34 @@
-const { ActivityType  } = require('discord.js');
+const { ActivityType } = require("discord.js");
 
 module.exports = (client) => {
-    client.user.setActivity({
-      name: "Hello world",
+  let status = [
+    {
+      name: "Não clique no link da live!",
       type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    })
-    console.log(`🤖✅ ${client.user.tag} esta online.`);
+      url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    },
+    {
+      name: "🔥Cloud 10x",
+    },
+    {
+      name: "Melhor Servidor de Rust💉",
+      type: ActivityType.Watching,
+    },
+    {
+      name: "Venha jogar! IP",
+      type: ActivityType.Listening,
+    },
+  ];
+
+    setInterval(() => {
+      let random = Math.floor(Math.random() * status.length);
+      client.user.setActivity(status[random]);
+    }, 10000);
+
+  // client.user.setActivity({
+  //   name: "Hello world",
+  //   type: ActivityType.Streaming,
+  //   url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  // })
+  console.log(`🤖✅ ${client.user.tag} esta online.`);
 };
