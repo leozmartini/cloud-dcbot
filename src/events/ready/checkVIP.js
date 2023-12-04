b0790cc17962890d2c5890f82d9051d030a47760
Leonardo de Martini <demartinileo@hotmail.com>
2023-12-04 15:12:10 -0300

    04/12

--- src/events/ready/checkVIP.js
+++ src/events/ready/checkVIP.js
@@ -2,8 +2,8 @@ const discord = require("discord.js");
 const vipsDB = require("../../database/vip");
 
 module.exports = (client) => {
-  const guild = client.guilds.cache.get('1071981233787129916')
-  const logChannel = client.channels.cache.get("1073394752109690970");
+  const guild = client.guilds.cache.get('1178503513819578429')
+  const logChannel = client.channels.cache.get("1181295005671895040");
 
   // canal de log
 
@@ -16,7 +16,7 @@ module.exports = (client) => {
           .deleteOne({ userID: vipsDBs[i].userID })
           .then(() => {
             const member = guild.members.cache.get(vipsDBs[i].userID);
-            member.roles.remove(vipsDBs[i].VIPRoleID) 
+            member.roles.remove(vipsDBs[i].VIPRoleID)
             console.log(`VIP REMOVIDO - ${vipsDBs[i].userTAG} - ${vipsDBs[i].userID}`)
           })
           .catch((error) => {
@@ -30,31 +30,33 @@ module.exports = (client) => {
 
       if (dateNow > vipsDBs[i].Fim) {
         let embed = new discord.EmbedBuilder()
-        .setColor("Red")
-        .setTitle(`${vipsDBs[i].userTAG} - ${vipsDBs[i].VIP}`)
-        .setDescription(
-          `userID: ${vipsDBs[i].userID} \n VIPRoleID: ${vipsDBs[i].VIPRoleID}`
-        )
-        .addFields(
-          { name: `Início:`, value: `${vipsDBs[i].Inicio}` },
-          { name: `Fim:`, value: `${vipsDBs[i].Fim}` }
-        );
+          .setColor("Red")
+          .setTitle(`${vipsDBs[i].userTAG} - ${vipsDBs[i].VIP}`)
+          .setDescription(
+            `userID: ${vipsDBs[i].userID} \n VIPRoleID: ${vipsDBs[i].VIPRoleID}`
+          )
+          .addFields(
+            { name: `Início:`, value: `${vipsDBs[i].Inicio}` },
+            { name: `Fim:`, value: `${vipsDBs[i].Fim}` }
+          );
 
         await removeVIP();
 
         logChannel.send({ content: `@here \n VIP REMOVIDO!`, embeds: [embed] });
 
       }
-      let data = new Date();
-      let dataFormatada = data.toLocaleString("pt-BR", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        day: "2-digit",
-        month: "2-digit",
-        year: "2-digit",
-      });
-      console.log(`Verificação de VIP concluida. ${dataFormatada}`);
+
     }
-  }, 3600000); // 1 hora
+    let data = new Date();
+    let dataFormatada = data.toLocaleString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    });
+    console.log(`Verificação de VIP concluida. ${dataFormatada}`);
+
+  }, 60000);
 };
--- src/index.js
+++ src/index.js
@@ -19,12 +19,15 @@ const client = new Client({
     mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.MONGODB_URL, { keepAlive: true })
     console.log('💾✅ Conectado com banco de dados.')
-    // eventHandler(client);
   } catch (error) {
     console.log(`💾❌ Erro ao conectar com banco de dados: ${error}`)
   }
-})()
-  eventHandler(client);
 
-client.login(process.env.TOKEN);
+
+})().then(() => {
+  eventHandler(client)
+  client.login(process.env.TOKEN);
+})
+
+
 
